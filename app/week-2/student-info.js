22f5d6a4723fb96b71ac497773ffe5e0e29cd071
CarterMcLeod1 <carter.mcleod@edu.sait.ca>
2026-01-15 15:30:30 -0700

    added github username

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   const name = "Carter McLeod";
-  const githubRepoUrl = "https://github.com/<your-username>/<your-repo>";
+  const githubRepoUrl = "https://github.com/CarterMcLeod1/cprg306-assignments";
 
   return (
     <section style={{ marginTop: "12px" }}>
